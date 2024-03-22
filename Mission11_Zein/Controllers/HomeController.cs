3b2dae41ba81ef4353999a072c571dafc54e63e7
zeinmusarsaa <zeinmusarsaa@gmail.com>
2024-03-22 11:26:57 -0600

    Add project files.

---
+++ Mission11_Zein/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Mission11_Zein.Models;
+using System.Diagnostics;
+
+namespace Mission11_Zein.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Mission11_Zein/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mission11_Zein.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
